c2275e5dfb7968e34e0d620902bfd2b85f84f2df
ismus <vanias9696@gmail.com>
2018-06-19 23:38:20 +0300

    8

---
+++ d1/ex08/ft_is_sort.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+	function ft_is_sort($arr)
+	{
+		$i = 0;
+		$s = $arr;
+		sort($s);
+		$n = count($arr);
+		while ($i < $n)
+		{
+			if ($s[$i] != $arr[$i])
+				return (FALSE) ;
+			$i++;
+		}
+		return (TRUE) ;
+	}
+?>
